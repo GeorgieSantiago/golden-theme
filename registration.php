df1aef0c72f021d1c6c627da14b73006f8be1215
RM Innovation <omidb@rminno.com>
2017-11-28 22:02:52 +0000

    init commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Theme Registration
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::THEME,
+	'frontend/RMI/Golden',
+	__DIR__
+);
